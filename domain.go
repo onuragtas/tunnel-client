9b2c1d87fea5f92592bb4642c0043755766962da
onuragtas <agtasonur@gmail.com>
2023-08-19 15:18:48 +0300

    tunnel changed

--- domain.go
+++ domain.go
@@ -7,7 +7,6 @@ import (
 	"github.com/onuragtas/tunnel-client/utils"
 	"log"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -53,145 +52,143 @@ func createDomain() {
 }
 
 func startTunnel(sshUser, sshPassword string) {
-	listDomain()
-	log.Println("Select IDs, Ex: 1:local_port:destination_ip,2:local_port:destination_ip...Or 1,2...\nClose:0")
-
-	if list == "" {
-		fmt.Scanf("%s", &list)
-	}
-
-	if list != "0" {
-		ids := strings.Split(list, ",")
-		for _, item := range ids {
-			var tunnel tunnel2.Client
-			splitted := strings.Split(item, ":")
-
-			item = splitted[0]
-			localPort := defaultLocalPort
-			localIp := defaultDestinationIp
-			if len(splitted) > 1 {
-				localPort = splitted[1]
-			}
-			if len(splitted) > 2 {
-				localIp = splitted[2]
-			}
-
-			c := make(chan int)
-
-			tunnelDetail := getTunnelItem(item)
-
-			if tunnelDetail != nil {
-				continue
-			}
-
-			domainDetail := getDomain(item)
-
-			// local service to be forwarded
-			destinationLocalPort, _ := strconv.Atoi(localPort)
-			var localEndpoint = tunnel2.Endpoint{
-				Host: localIp,
-				Port: destinationLocalPort,
-			}
-
-			// remote SSH server
-			var serverEndpoint = tunnel2.Endpoint{
-				Host: domainDetail.Domain,
-				Port: 22,
-			}
-
-			// remote forwarding port (on remote SSH server network)
-			var remoteEndpoint = tunnel2.Endpoint{
-				Host: domainDetail.Domain,
-				Port: domainDetail.Port,
-			}
-
-			tunnel.LocalEndpoint = localEndpoint
-			tunnel.RemoteEndpoint = remoteEndpoint
-			tunnel.ServerEndpoint = serverEndpoint
-			tunnel.Signal = c
-			tunnel.CloseHandleSignal = closeHandleSignal
-			tunnel.SshUser = sshUser
-			tunnel.SshPassword = sshPassword
-
-			startedTunnels.Data = append(startedTunnels.Data, Item{Signal: closeHandleSignal, CloseSignal: c, Domain: domainDetail, KeepAliveTime: time.Now(), Tunnel: tunnel})
-
-			go tunnel.Connect()
-		}
-	}
-
-	go listenClose()
+	//listDomain()
+	//log.Println("Select IDs, Ex: 1:local_port:destination_ip,2:local_port:destination_ip...Or 1,2...\nClose:0")
+	//
+	//if list == "" {
+	//	fmt.Scanf("%s", &list)
+	//}
+	//
+	//if list != "0" {
+	//	ids := strings.Split(list, ",")
+	//	for _, item := range ids {
+	//		var tunnel tunnel2.Client
+	//		splitted := strings.Split(item, ":")
+	//
+	//		item = splitted[0]
+	//		localPort := defaultLocalPort
+	//		localIp := defaultDestinationIp
+	//		if len(splitted) > 1 {
+	//			localPort = splitted[1]
+	//		}
+	//		if len(splitted) > 2 {
+	//			localIp = splitted[2]
+	//		}
+	//
+	//		c := make(chan int)
+	//
+	//		tunnelDetail := getTunnelItem(item)
+	//
+	//		if tunnelDetail != nil {
+	//			continue
+	//		}
+	//
+	//		domainDetail := getDomain(item)
+	//
+	//		// local service to be forwarded
+	//		destinationLocalPort, _ := strconv.Atoi(localPort)
+	//		var localEndpoint = tunnel2.Endpoint{
+	//			Host: localIp,
+	//			Port: destinationLocalPort,
+	//		}
+	//
+	//		// remote SSH server
+	//		var serverEndpoint = tunnel2.Endpoint{
+	//			Host: domainDetail.Domain,
+	//			Port: 22,
+	//		}
+	//
+	//		// remote forwarding port (on remote SSH server network)
+	//		var remoteEndpoint = tunnel2.Endpoint{
+	//			Host: domainDetail.Domain,
+	//			Port: domainDetail.Port,
+	//		}
+	//
+	//		tunnel.LocalEndpoint = localEndpoint
+	//		tunnel.RemoteEndpoint = remoteEndpoint
+	//		tunnel.ServerEndpoint = serverEndpoint
+	//		tunnel.Signal = c
+	//		tunnel.CloseHandleSignal = closeHandleSignal
+	//		tunnel.SshUser = sshUser
+	//		tunnel.SshPassword = sshPassword
+	//
+	//		startedTunnels.Data = append(startedTunnels.Data, Item{Signal: closeHandleSignal, CloseSignal: c, Domain: domainDetail, KeepAliveTime: time.Now(), Tunnel: tunnel})
+	//
+	//		go tunnel.Connect()
+	//	}
+	//}
+	//
+	//go listenClose()
 }
 func renewDomain() {
-	var id string
-	listDomain()
-	log.Println("Select ID, Ex: 1\nClose:0")
-
-	if id == "" {
-		fmt.Scanf("%s", &id)
-	}
-
-	if id != "0" {
-		domainDetail := getDomain(id)
-
-		requestClient.RenewDomain(utils.ReadToken(), domainDetail.Domain)
-	}
+	//var id string
+	//listDomain()
+	//log.Println("Select ID, Ex: 1\nClose:0")
+	//
+	//if id == "" {
+	//	fmt.Scanf("%s", &id)
+	//}
+	//
+	//if id != "0" {
+	//	domainDetail := getDomain(id)
+	//
+	//	requestClient.RenewDomain(utils.ReadToken(), domainDetail.Domain)
+	//}
 
 }
 
 func closeTunnel() {
-	listDomain()
-	log.Println("Select IDs, Ex: 1,2,3,4\nClose:0")
-
-	if list == "" {
-		fmt.Scanf("%s", &list)
-	}
-
-	if list != "0" {
-		ids := strings.Split(list, ",")
-		for _, item := range ids {
-			tunnelDetail := getTunnelItem(item)
-			if tunnelDetail != nil {
-				tunnelDetail.CloseSignal <- 1
-				removeTunnelItem(item)
-			}
-		}
-	}
+	//listDomain()
+	//log.Println("Select IDs, Ex: 1,2,3,4\nClose:0")
+	//
+	//if list == "" {
+	//	fmt.Scanf("%s", &list)
+	//}
+	//
+	//if list != "0" {
+	//	ids := strings.Split(list, ",")
+	//	for _, item := range ids {
+	//		tunnelDetail := getTunnelItem(item)
+	//		if tunnelDetail != nil {
+	//			tunnelDetail.CloseSignal <- 1
+	//			removeTunnelItem(item)
+	//		}
+	//	}
+	//}
 }
 
 func deleteDomain() {
-	listDomain()
-	log.Println("Select IDs, Ex: 1,2,3,4\nClose: 0")
-
-	var idList []string
-
-	if list == "" {
-		fmt.Scanf("%s", &list)
-	}
-
-	if list != "0" {
-		ids := strings.Split(list, ",")
-		for _, item := range ids {
-			idList = append(idList, strconv.Itoa(getDomain(item).ID))
-		}
-		fmt.Println(requestClient.DeleteDomain(utils.ReadToken(), idList))
-	}
+	//listDomain()
+	//log.Println("Select IDs, Ex: 1,2,3,4\nClose: 0")
+	//
+	//var idList []string
+	//
+	//if list == "" {
+	//	fmt.Scanf("%s", &list)
+	//}
+	//
+	//if list != "0" {
+	//	ids := strings.Split(list, ",")
+	//	for _, item := range ids {
+	//		idList = append(idList, strconv.Itoa(getDomain(item).ID))
+	//	}
+	//	fmt.Println(requestClient.DeleteDomain(utils.ReadToken(), idList))
+	//}
 
 }
 
-func getDomain(id string) models.DomainItem {
+func getDomain(id int) models.DomainItem {
 	for key, item := range domainList.Data.Domains {
-		idCasting, _ := strconv.Atoi(id)
-		if key == idCasting-1 {
+		if key == id-1 {
 			return item
 		}
 	}
 	return models.DomainItem{}
 }
 
-func getTunnelItem(id string) *Item {
+func getTunnelItem(id int) *Item {
 	for key, item := range startedTunnels.Data {
-		idCasting, _ := strconv.Atoi(id)
-		if key == idCasting-1 {
+		if key == id-1 {
 			return &item
 		}
 	}
